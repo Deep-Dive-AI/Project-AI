8b2b1e8f1f95748f8b76b36476a03ba13a00a048
micha de haan <130181637+MichadeHaan@users.noreply.github.com>
2024-11-13 13:08:36 +0100

    database update

--- public/login/sql/user.sql
+++ public/login/sql/user.sql
@@ -4,5 +4,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    reset_token VARCHAR(255) DEFAULT NULL,
+    token_expiration DATETIME DEFAULT NULL
 );
